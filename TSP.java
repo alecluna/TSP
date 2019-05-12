e12cf5d575ff96a996da1fe1aac0bf388d7c39a7
Alec Luna <alecluna@csus.edu>
2019-05-12 15:52:29 -0700

    small changes to naming

--- TSP.java
+++ TSP.java
@@ -3,10 +3,25 @@ import java.io.*;
 import java.lang.*;
 
 class TSP {
+    int totalVertices, E;
+    Edge edge[];
+
+    TSP(int vertices, int edges) {
+        totalVertices = vertices;
+        E = edges;
+        edge = new Edge[E];
+        for (int i = 0; i < edges; ++i)
+            edge[i] = new Edge();
+    }
+
+    class VerticeGroups {
+        int rank, parent;
+    };
+
     class Edge implements Comparable<Edge> {
         int source, destination, weight;
 
-        // compartor function to sort weights, TODO refactor
+        // comparater function to sort weights, TODO refactor
         public int compareTo(Edge nextWeight) {
             if (this.weight == nextWeight.weight)
                 return 0;
@@ -17,26 +32,8 @@ class TSP {
         }
     };
 
-    // A class to represent a subset for union-find
-    class verticesGroups {
-        int parent, rank;
-    };
-
-    int V, E; // V-> no. of vertices & E->no.of edges
-    Edge edge[]; // collection of all edges
-
-    // Creates a TSP with V vertices and E edges
-    TSP(int v, int e) {
-        V = v;
-        E = e;
-        edge = new Edge[E];
-        for (int i = 0; i < e; ++i)
-            edge[i] = new Edge();
-    }
-
-    // A utility function to find set of an element i
-    // (uses path compression technique)
-    int find(verticesGroups subsets[], int i) {
+    // path compression
+    int find(VerticeGroups subsets[], int i) {
         // find root and make root as parent of i (path compression)
         if (subsets[i].parent != i)
             subsets[i].parent = find(subsets, subsets[i].parent);
@@ -46,7 +43,7 @@ class TSP {
 
     // A function that does union of two sets of x and y
     // (uses union by rank)
-    void Union(verticesGroups subsets[], int x, int y) {
+    void Union(VerticeGroups subsets[], int x, int y) {
         int xroot = find(subsets, x);
         int yroot = find(subsets, y);
 
@@ -66,24 +63,22 @@ class TSP {
     }
 
     public void KruskalMST() {
-        Edge result[] = new Edge[V]; // expected output
+        // instantiate an array of results
+        Edge result[] = new Edge[totalVertices]; // expected output
         int e = 0; // An index variable, used for result[]
         int i = 0; // An index variable, used for sorted edges
-        for (i = 0; i < V; ++i)
+        for (i = 0; i < totalVertices; ++i)
             result[i] = new Edge();
 
-        // Step 1: Sort all the edges in non-decreasing order of their
-        // weight. If we are not allowed to change the given TSP, we
-        // can create a copy of array of edges
         Arrays.sort(edge);
 
         // Allocate memory for creating V subsets
-        verticesGroups subsets[] = new verticesGroups[V];
-        for (i = 0; i < V; ++i)
-            subsets[i] = new verticesGroups();
+        VerticeGroups subsets[] = new VerticeGroups[totalVertices];
+        for (i = 0; i < totalVertices; ++i)
+            subsets[i] = new VerticeGroups();
 
         // Create V subsets with single elements
-        for (int v = 0; v < V; ++v) {
+        for (int v = 0; v < totalVertices; ++v) {
             subsets[v].parent = v;
             subsets[v].rank = 0;
         }
@@ -91,7 +86,7 @@ class TSP {
         i = 0; // Index used to pick next edge
 
         // Number of edges to be taken is equal to V-1
-        while (e < V - 1) {
+        while (e < totalVertices - 1) {
             // Step 2: Pick the smallest edge. And increment
             // the index for next iteration
             Edge next_edge = new Edge();
@@ -128,17 +123,18 @@ class TSP {
 
         try (Scanner reader = new Scanner(new File("input.txt"))) {
 
-            int V = Integer.parseInt(reader.next());
+            int vertices = Integer.parseInt(reader.next());
 
-            if (V == 0) {
-                System.out.println("0 Vertices given...exiting program");
+            if (vertices == 0) {
+                System.out.println("0 Vertices given ... Terminating program");
+                System.exit(0);
             }
 
-            int E = V + 1;
-            System.out.println("Vertices V = " + V);
-            System.out.println("Edges E = " + E);
+            int edges = vertices + 1;
+            System.out.println("Vertices V = " + vertices);
+            System.out.println("Edges = " + edges);
 
-            TSP TSP = new TSP(V, E);
+            TSP TSP = new TSP(vertices, edges);
             int counter = 0;
             while (reader.hasNext()) {
 
